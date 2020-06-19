966ebbb00bb522f5705e3a850a4ba1d27c17f89c
Peter Grimm <pagrimm@gmail.com>
2020-06-19 12:10:55 -0700

    add function for displaying cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 //Business logic
 //Cart constructor and prototypes
-function Cart(tax, deliveryFee) {
+function Cart(salesTax, deliveryFee) {
   this.pizzas = [],
   this.subTotal = 0,
-  this.tax = tax,
+  this.salesTax = salesTax,
+  this.totalTax = 0,
   this.deliveryFee = deliveryFee,
   this.totalPrice = 0
 }
 
 Cart.prototype.calculatePrice = function() {
   for (i = 0; i < this.pizzas.length; i++) {
-    this.pizzas[i].calculatePrice();
     this.subTotal += this.pizzas[i].price;
   }
-  this.totalPrice = this.subTotal + (this.subTotal * this.tax) + this.deliveryFee;
+  this.totalTax = this.subTotal * this.salesTax;
+  this.totalPrice = this.subTotal + this.totalTax + this.deliveryFee;
 }
 
 //Pizza constructor and prototypes
@@ -37,8 +38,24 @@ function PizzaOption (name, value) {
   this.value = value;
 }
 
-
 //UI logic
+function displayCart(cart) {
+  $(".cart-container").html("");
+  for (i = 0; i < cart.pizzas.length; i++) {
+    $(".cart-container").prepend($(".pizza-cart-container").last().clone());
+    $(".pizza-name-cart").first().text("Pizza #" + (i + 1));
+    $(".pizza-size-cart").first().text(cart.pizzas[i].size.name);
+    $(".pizza-cheese-cart").first().text(cart.pizzas[i].cheese.name);
+    for (n = 0; n < cart.pizzas[i].toppings.length; n++) {
+      $(".pizza-toppings-cart").first().append("<div>" + cart.pizzas[i].toppings[n].name + "</div>");
+    }
+  }
+  $(".subtotal-cart").text("Subtotal: $" + cart.subTotal.toFixed(2));
+  $(".tax-cart").text("Tax: $" + cart.totalTax.toFixed(2));
+  $(".delivery-fee-cart").text("Delivery fee: $" + cart.deliveryFee.toFixed(2));
+  $(".price-cart").text("Total Price: $" + cart.totalPrice.toFixed(2));
+}
+
 function getInputs() {
   let inputs = [];
   let size = new PizzaOption($("label[for='" + $("input:radio[name=size]:checked").attr("id") + "']").text(), parseInt($("input:radio[name=size]:checked").val()));
@@ -53,14 +70,16 @@ function getInputs() {
 }
 
 $(document).ready(function () {
-  let tax = .065;
+  let salesTax = .065;
   let deliveryFee = 3;
-  let cart = new Cart(tax, deliveryFee);
+  let cart = new Cart(salesTax, deliveryFee);
   $("#pizza-input-form").submit(function (event) {
     event.preventDefault();
     let inputs = getInputs();
     let pizza = new Pizza(inputs[0], inputs[1], inputs[2])
+    pizza.calculatePrice();
     cart.pizzas.push(pizza);
     cart.calculatePrice();
+    displayCart(cart);
   });
 });
